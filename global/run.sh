52c94c5657266d543efafe5379348092eb82b8e8
evangowan <evangowan@gmail.com>
2019-05-06 17:19:17 +0200

    set parameters for North America 70000 year run, increased the shear stress in the labrador and northern Ontario regions, since the lake tilts indicate more ice is needed

--- global/run.sh
+++ global/run.sh
@@ -29,15 +29,15 @@ seq ${max_time} -${interval} 0 > times_to_calculate
 
 # for sanity sake, I'm setting it up to do one region at a time, and combine them afterwards
 
-#region=North_America
+region=North_America
 #region=Eurasia
-region=Antarctica
+#region=Antarctica
 #region=Patagonia
 
 # information that will be put into the file ${region}/run_info.txt
 # !!!!!!!! very important !!!!!!!!!, change this number for every run, the run number will be used to identify a GIA deformation run!
 # also include your name, this will make it possible to distinguish run numbers from different authors
-run_number="22" 
+run_number="23" 
 your_name="Evan" # no spaces or underscores!
 run_description="First run of 70000 year reconstruction at 2500 year intervals, no GIA"
 
