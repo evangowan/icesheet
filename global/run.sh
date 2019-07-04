da68c956f48d81491c616b70da2a544b7c1899c1
evangowan <evangowan@gmail.com>
2019-07-04 08:32:01 +0200

    parameters for Eurasian run, less ice in many areas

--- global/run.sh
+++ global/run.sh
@@ -29,15 +29,15 @@ seq ${max_time} -${interval} 0 > times_to_calculate
 
 # for sanity sake, I'm setting it up to do one region at a time, and combine them afterwards
 
-region=North_America
-#region=Eurasia
+#region=North_America
+region=Eurasia
 #region=Antarctica
 #region=Patagonia
 
 # information that will be put into the file ${region}/run_info.txt
 # !!!!!!!! very important !!!!!!!!!, change this number for every run, the run number will be used to identify a GIA deformation run!
 # also include your name, this will make it possible to distinguish run numbers from different authors
-run_number="56" 
+run_number="57" 
 your_name="Evan" # no spaces or underscores!
 run_description="Adjusted the shear stress downwards in regions where sea level is way too high"
 
