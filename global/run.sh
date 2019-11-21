8fb5956df3dfc5e93eb07f0f8c0188dd9daa0fe6
evangowan <evangowan@gmail.com>
2019-11-21 16:09:56 +0100

    Patagonia run number 81

--- global/run.sh
+++ global/run.sh
@@ -31,15 +31,15 @@ seq ${max_time} -${interval} 0 > times_to_calculate
 
 #region=North_America
 #region=Eurasia
-region=Antarctica
-#region=Patagonia
+#region=Antarctica
+region=Patagonia
 
 # information that will be put into the file ${region}/run_info.txt
 # !!!!!!!! very important !!!!!!!!!, change this number for every run, the run number will be used to identify a GIA deformation run!
 # also include your name, this will make it possible to distinguish run numbers from different authors
-run_number="80" 
+run_number="81" 
 your_name="Evan" # no spaces or underscores!
-run_description="Antarctica run with the larger extent MIS 3 Laurentide signal"
+run_description="Patagonia run with the larger extent MIS 3 Laurentide signal"
 
 
 # if you want to use an alternative margin chronology, turn this switch on. By default, it searches for a margin in the folder, and if it is not found, it will
